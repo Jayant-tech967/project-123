34992521431ce7e01ca1e7788ea238574e19606c
Jayant-tech967 <81297130+Jayant-tech967@users.noreply.github.com>
2021-08-12 21:35:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightwristX=0;
+leftwristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+    canvas.position(580,88);
+}
